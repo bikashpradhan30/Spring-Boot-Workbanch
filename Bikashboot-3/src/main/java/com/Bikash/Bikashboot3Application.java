2cccac89f4d7f3ec1c9da3d71bff0a7dee3b52e2
Bikash <bikashgp748768@gmail.com>
2025-05-06 17:45:55 +0530

    It Will be the Dev Tool Program

---
+++ Bikashboot-3/src/main/java/com/Bikash/Bikashboot3Application.java
@@ -0,0 +1,15 @@
+package com.Bikash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bikashboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bikashboot3Application.class, args);
+		System.out.println("Helloworld .......");
+    	System.out.println("Byeworld .......");
+	}
+
+}
