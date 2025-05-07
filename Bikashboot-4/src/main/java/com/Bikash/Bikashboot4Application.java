7f0d92c7836d32cbed44f07fa436b3b76e7a8626
Bikash <bikashgp748768@gmail.com>
2025-05-07 13:53:48 +0530

    Using of spring.io.initializr & crtea program

---
+++ Bikashboot-4/src/main/java/com/Bikash/Bikashboot4Application.java
@@ -0,0 +1,18 @@
+package com.Bikash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bikashboot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bikashboot4Application.class, args);
+		System.out.println("Hii..");
+		System.out.println("Hello..");
+		System.out.println("Bey..");
+		int a =5019;
+		System.out.println("This is the Redg. No ::"+a);
+	}
+
+}
