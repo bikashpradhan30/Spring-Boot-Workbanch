42bd5f174b94cf2260bc3492c7df637283ea7ec1
Bikash <bikashgp748768@gmail.com>
2025-05-10 23:54:03 +0530

    It is an another imp program file

---
+++ Bikashboot-8/src/main/java/com/CoolCoder/Bikashboot8Application.java
@@ -0,0 +1,13 @@
+package com.CoolCoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bikashboot8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bikashboot8Application.class, args);
+	}
+
+}
---
+++ Bikashboot-8/src/test/java/com/CoolCoder/Bikashboot8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoolCoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bikashboot8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
